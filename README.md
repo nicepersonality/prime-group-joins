1e34d176c761389c5461ffc3bac65bde46838922
Lilith Bentley <sbentley@gmail.com>
2019-10-01 11:23:36 -0500

    Complete first four queries

--- README.md
+++ README.md
@@ -17,10 +17,10 @@ SELECT * FROM customers;
 ```
 
 ## Tasks
-1. [ ] Get all customers and their addresses.
-2. [ ] Get all orders and their line items (orders, quantity and product).
-3. [ ] Which warehouses have cheetos?
-4. [ ] Which warehouses have diet pepsi?
+1. [X] Get all customers and their addresses.
+2. [X] Get all orders and their line items (orders, quantity and product).
+3. [X] Which warehouses have cheetos?
+4. [X] Which warehouses have diet pepsi?
 5. [ ] Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 6. [ ] How many customers do we have?
 7. [ ] How many products do we carry?
--- joins-solution.sql
+++ joins-solution.sql
@@ -1,9 +1,28 @@
 -- Tasks
 
 --  1. Get all customers and their addresses.
+SELECT "customers".first_name, "customers".last_name, "address_type", "street", "city", "state", "zip" FROM "addresses"
+JOIN "customers" ON "customers".id = "addresses".customer_id
+ORDER BY "customers".last_name;
+
 --  2. Get all orders and their line items (orders, quantity and product).
+SELECT "orders".id, "orders".order_date, "quantity", "products".description FROM "line_items"
+JOIN "orders" ON "orders".id = "line_items".order_id
+JOIN "products" ON "products".id = "line_items".product_id
+ORDER BY "orders".order_date, "orders".id;
+
 --  3. Which warehouses have cheetos?
+SELECT "warehouse".warehouse, "products".description, "on_hand" FROM "warehouse_product"
+JOIN "warehouse" ON "warehouse".id = "warehouse_product".warehouse_id
+JOIN "products" ON "products".id = "warehouse_product".product_id
+WHERE "products".description = 'cheetos' AND "on_hand" > 0;
+
 --  4. Which warehouses have diet pepsi?
+SELECT "warehouse".warehouse, "products".description, "on_hand" FROM "warehouse_product"
+JOIN "warehouse" ON "warehouse".id = "warehouse_product".warehouse_id
+JOIN "products" ON "products".id = "warehouse_product".product_id
+WHERE "products".description = 'diet pepsi' AND "on_hand" > 0;
+
 --  5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 --  6. How many customers do we have?
 --  7. How many products do we carry?
